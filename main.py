b7a8a50156e92c2317fadb686afadacf62ce6c14
kk-deng <71301799+kk-deng@users.noreply.github.com>
2021-12-21 22:29:09 -0500

    Split spiders into two for each domain

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ Created on 2021-02-08 16:01:50
 @author: Boris
 """
 
-from spiders import *
+from spiders import spider_man, bestbuy_spider
 
 if __name__ == "__main__":
     spider = spider_man.RfdSpider()
+    bb_spider = bestbuy_spider.BBSpider()
     spider.start()
+    bb_spider.start()
