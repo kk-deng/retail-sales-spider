d91c063337fa5275808338074b337f04a54d1b07
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-01-03 17:59:55 -0500

    Added more tests

--- tests/test_rfdtopic/test_rfdtopic.py
+++ tests/test_rfdtopic/test_rfdtopic.py
@@ -1,6 +1,13 @@
 from datetime import datetime
 
 
+def test_votes(api_topic):
+    assert type(api_topic.upvotes) is int
+
+def test_views(api_topic):
+    assert api_topic.total_views == 32290
+    assert api_topic.total_replies == 213
+
 def test_topic_link(api_topic):
     topic_title_link = api_topic.topic_title_link
     print(topic_title_link)
@@ -9,3 +16,9 @@ def test_topic_link(api_topic):
 def test_topic_post_time(api_topic):
     assert type(api_topic.post_time) is datetime
     assert api_topic.post_time_str == "12-16 17:05:26"
+
+def test_offer(api_topic):
+    assert type(api_topic.dealer_name) is str
+    assert api_topic.offer_price == '42.50'
+    assert "https://" in api_topic.offer_url
+    assert api_topic.offer_expires_at == None
