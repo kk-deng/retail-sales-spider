02d69461e3b1310187dd00148845da5c72b5bb3a
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-01-19 00:30:21 -0500

    Added unittest of multiple attributes

--- tests/test_spiders/test_ikea.py
+++ tests/test_spiders/test_ikea.py
@@ -9,3 +9,11 @@ def test_ikea_item_cls(api_ikea_products):
     product = ikea_spider.IkeaProduct(ikea_product)
 
     assert type(product.product_id) == int
+    assert len(product.store_id) == 3
+    assert type(product.sale_point) == str
+    assert product.status_code in ['HIGH_IN_STOCK', 'OUT_OF_STOCK', 'LOW_IN_STOCK']
+    if product.status_code == 'OUT_OF_STOCK':
+        assert product.stock_num == 0
+    else:
+        assert product.stock_num > 0
+        
