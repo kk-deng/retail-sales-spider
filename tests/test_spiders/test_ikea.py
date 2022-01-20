719123fe63922aa8475ea8ed90825ce5ac7bdaf3
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-01-20 00:41:35 -0500

    Updated unit test for restock_date

--- tests/test_spiders/test_ikea.py
+++ tests/test_spiders/test_ikea.py
@@ -15,10 +15,15 @@ def test_ikea_item_cls(api_ikea_products_by_store):
         assert product.stock_num == 0
     else:
         assert product.stock_num > 0
-        
+    
+    assert type(product.items) == dict
+
+    # Assert the "Estimated back in stock: <b>2022-01-21</b>"
+    assert product.restock_date == '2022-01-21'
+
 
 @pytest.mark.ikeaitem
-def test_ikea_item_cls():
+def test_ikea_item_spider():
     spider = ikea_spider.IkeaSpider()
     assert '79009502' in spider.products_dict
     assert type(spider.id_url_str) == str
