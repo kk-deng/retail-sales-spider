9e786df71a739734743c370f76cc72f8d2fe4e60
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-03-11 01:25:03 -0500

    Initial release of Ikea item

---
+++ items/ikea_item.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2022-03-11 1:23:36
+---------
+@summary: Ikea item for database loading
+---------
+@author: Kelvin
+"""
+
+from feapder import UpdateItem
+
+
+class IkeaStockItem(UpdateItem):
+    """Ikea stock information for database storage, e.g. MongoDB"""
+
+    def __init__(self, *args, **kwargs):
+        pass
