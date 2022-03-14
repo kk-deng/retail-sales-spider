05eb68ac6460a77e8b7756717689096841058603
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-03-13 22:15:59 -0400

    Updated attributes of IkeaStockItem class

--- items/ikea_item.py
+++ items/ikea_item.py
@@ -15,11 +15,13 @@ class IkeaStockItem(UpdateItem):
 
     def __init__(self, ikea_product, *args, **kwargs):
         self.product_id = ikea_product.product_id
-        self.product_type = ikea_product.product_type
+        self.title = ikea_product.title
+        self.status_code = ikea_product.status_code
         self.stock_num = ikea_product.stock_num
         self.store_id = ikea_product.store_id
+        self.restock_date = ikea_product.restock_date
+        self.product_type = ikea_product.product_type
         self.sale_point = ikea_product.sale_point
         self.status = ikea_product.status
-        self.status_code = ikea_product.status_code
         self.store_name = ikea_product.store_name
         self.locations = ikea_product.locations
