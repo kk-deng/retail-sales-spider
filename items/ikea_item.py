e950da0627922d4cce4698446c027440c342fe2e
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-03-14 21:58:43 -0400

    Parsed product name from ikea_product instance

--- items/ikea_item.py
+++ items/ikea_item.py
@@ -14,10 +14,17 @@ class IkeaStockItem(UpdateItem):
     """Ikea stock information for database storage, e.g. MongoDB"""
 
     def __init__(self, ikea_product, *args, **kwargs):
+        # art_id_dict = kwargs.get('ref_dict')[0]
+        # spr_id_dict = kwargs.get('ref_dict')[1]
+        # art_id_dict.update(spr_id_dict)
+
+        product_id_dict = kwargs.get('ref_dict')
+
         self.product_id = ikea_product.product_id
-        self.title = ikea_product.title
+        self.name = product_id_dict.get(self.product_id)['title']
         self.status_code = ikea_product.status_code
         self.stock_num = ikea_product.stock_num
+        self.timestamp = ikea_product.timestamp
         self.store_id = ikea_product.store_id
         self.restock_date = ikea_product.restock_date
         self.product_type = ikea_product.product_type
