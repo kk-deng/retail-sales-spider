6da7415ce068ff25588252032fe0022f8e8111b8
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-03-21 01:12:53 -0400

    Added send_bot_msg method for TelegramBot class

--- utils/tg_bot.py
+++ utils/tg_bot.py
@@ -1,16 +1,20 @@
 import telegram
+from functools import wraps
 from feapder.utils.log import log
+from datetime import time
+
 
 class TelegramBot:
-    def __init__(self, token):
+    def __init__(self, token, chat_id):
         self.token = token
+        self.chat_id = chat_id
         self.bot = telegram.Bot(token=self.token)
 
     def send_action(action):
         def decorator(func):
             @wraps(func)
             def command_func(self, *args, **kwargs):
-                self.bot.send_action(chat_id=self.file_operator.chat_id, action=action)
+                self.bot.send_chat_action(chat_id=self.chat_id, action=action)
 
                 return func(self, *args, **kwargs)
             
@@ -20,8 +24,27 @@ class TelegramBot:
 
     @send_action(telegram.ChatAction.TYPING)
     def send_bot_msg(
-        self, content_msg: str, 
-        reply_to_msg_id: str or None
+        self, 
+        content_msg: str, 
+        reply_to_msg_id: str or None=None
     ) -> str or bool:
         log_content = content_msg.replace("\n", "")
         log.warning(f'## Sending: {log_content}')
+
+        try:
+            returned_msg = self.bot.send_message(
+                text=content_msg, 
+                chat_id=self.chat_id,
+                reply_to_message_id=reply_to_msg_id,
+                # reply_markup=reply_markup,
+                parse_mode=telegram.ParseMode.MARKDOWN
+                )
+            
+            log.info('## Msg was sent successfully!')
+            time.sleep(3)
+
+            return returned_msg
+        except Exception as e:
+            log.warning(f'## Msg failed sending with error:\n{e}')
+
+            return False
