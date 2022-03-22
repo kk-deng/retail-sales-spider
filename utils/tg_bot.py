6c813212011cd4070ce9e0f98d8509b82c617a3a
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-03-21 21:33:19 -0400

    Added docstring for send_bot_msg method

--- utils/tg_bot.py
+++ utils/tg_bot.py
@@ -1,7 +1,7 @@
 import telegram
 from functools import wraps
 from feapder.utils.log import log
-from datetime import time
+import time
 
 
 class TelegramBot:
@@ -27,7 +27,16 @@ class TelegramBot:
         self, 
         content_msg: str, 
         reply_to_msg_id: str or None=None
-    ) -> str or bool:
+    ) -> telegram.Message or bool:
+        """Given a string msg and msg_id, send msg to telegram chat_id.
+
+        Args:
+            content_msg (str): The string msg to send to telegram.
+            reply_to_msg_id (str or None, optional): The msg_id to be quoted when sending msg. Defaults to None.
+
+        Returns:
+            telegram.Message or bool: Return a Message class or False
+        """
         log_content = content_msg.replace("\n", "")
         log.warning(f'## Sending: {log_content}')
 
@@ -47,4 +56,4 @@ class TelegramBot:
         except Exception as e:
             log.warning(f'## Msg failed sending with error:\n{e}')
 
-            return False
+            raise Exception
