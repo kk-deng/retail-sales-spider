7ed05872259b3d7d2e4cdf952276198a78875e60
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-06-20 22:58:46 -0400

    Created a func to load json file and return dict Updated unittest of RFD topic to new structure

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,57 +6,14 @@ from spiders.bestbuy_spider import BestBuyItem
 from spiders.spider_man import RfdTopic
 
 
+def load_json(file_path) -> dict:
+    with open(file_path) as json_file:
+        return json.load(json_file)
+
+
 @pytest.fixture(autouse=True)
 def api_topic():
-    info = {
-        'topic_id': 2512445, 
-        'forum_id': 9, 
-        'title': 'Fido Internet 150Mps service for 42.50  (first 12 months)', 
-        'slug': 'fido-fido-internet-150mps-service-42-50-first-12-months', 
-        'author_id': 21329, 
-        'is_subscribed': False, 
-        'can_subscribe': False, 
-        'can_report': False, 
-        'can_reply': False, 
-        'is_poll': False, 
-        'status': 0, 
-        'type': 0, 
-        'post_time': '2021-12-16T22:05:26+00:00', 
-        'last_post_time': '2022-01-03T20:53:40+00:00', 
-        'first_post_id': 35448857, 
-        'last_post_id': 35533982, 
-        'total_replies': 213, 
-        'has_new_post': False, 
-        'total_views': 32290, 
-        'visibility': 1, 
-        'has_replied': False, 
-        'attachments': [], 
-        'shadow_type': 0, 
-        'votes': {
-            'total_up': 50, 
-            'total_down': 2, 
-            'current_vote': 0, 
-            'can_vote_up': False, 
-            'can_vote_down': False
-            }, 
-        'prefix': None, 
-        'summary': {
-            'can_edit': False, 
-            'body': ''
-        }, 
-        'trader_type': None, 
-        'web_path': '/fido-fido-internet-150mps-service-42-50-first-12-months-2512445/', 
-        'offer': {
-            'category_id': 5125, 
-            'dealer_id': 158, 
-            'dealer_name': 'Fido', 
-            'url': 'https://www.fido.ca/internet/packages', 
-            'price': '42.50', 
-            'savings': '50% off', 
-            'expires_at': None, 
-            'holiday': 0
-        }
-    }
+    info = load_json('tests/jsons/rfd_topic.json')
 
     watch_list = [
         'PS5',
@@ -1114,7 +1071,6 @@ def api_costco_item():
 
 @pytest.fixture
 def api_lenovo_items():
-    with open('tests/jsons/lenovo.json') as lenovo_file:
-        data = json.load(lenovo_file)
+    data = load_json('tests/jsons/lenovo.json')
         
     return data
