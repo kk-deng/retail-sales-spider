41dcfc63b6e5b54c98634d716d34ed9c0562763d
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-06-23 22:12:13 -0400

    Initial release of lenovo spider

---
+++ spiders/lenovo_spider.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2022-06-22 16:06:12
+---------
+@summary:
+---------
+@author: Kelvin
+"""
+from __future__ import annotations
+
+import random
+import time
+from datetime import datetime
+from datetime import time as tm
+
+import feapder
+from feapder.utils.log import log
+from pydantic import BaseModel
+from tools import file_input_output
+from utils.tg_bot import TelegramBot
+
+SCRAPE_COUNT = 3000
+
+
+class LenovoSpider(feapder.AirSpider):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file_operator = file_input_output.FileReadWrite()
+        self.chat_id = self.file_operator.chat_id
+        self.tg_token = self.file_operator.newbot_token
+        self.bot = TelegramBot(token=self.tg_token, chat_id=self.chat_id, parse_mode=False)
+        self.lenovo_header = self.file_operator.get_spider_header('lenovo_header')
+        self.url = "https://openapi.lenovo.com/ca/en/ofp/search/dlp/product/query/get/_tsc"
+        self.params = {
+            "pageFilterId": "49c9f88b-069a-41c9-9785-643e6aab7e96",
+            "subseriesCode": "LEN101T0037",
+            "params": r"%7B%22classificationGroupIds%22%3A%22800001%22%2C%22pageFilterId%22%3A%2249c9f88b-069a-41c9-9785-643e6aab7e96%22%2C%22facets%22%3A%5B%5D%2C%22page%22%3A%221%22%2C%22pageSize%22%3A%2220%22%2C%22boostsProductCodes%22%3A%22%22%2C%22init%22%3Atrue%2C%22sorts%22%3A%5B%22shippingDate%22%5D%2C%22subseriesCode%22%3A%22LEN101T0037%22%7D"
+        }
+        self.previous_model = Z16Model()
+
+    def start_requests(self):
+        for i in range(1, SCRAPE_COUNT):
+            time_gap = self.get_random_time_gap()
+        
+            yield feapder.Request(self.url, params=self.params, method="GET")
+            
+            if SCRAPE_COUNT > 2:
+                log.info(f'## Lenovo.ca running for {i} / {SCRAPE_COUNT} runs, waiting for {time_gap}s...')
+                time.sleep(time_gap)
+    
+    def validate(self, request, response):
+        if response.status_code != 200:
+            content_msg = f'Error: HTTP {response.status_code}, pausing...'
+            self.bot.send_bot_msg(content_msg)
+            # raise Exception("response code not 200")
+
+    def download_midware(self, request):
+        request.headers = self.lenovo_header
+        return request
+
+    def parse(self, request, response):
+        api_json = response.json
+
+        # Only check the highest spec HUS model
+        hus_info = api_json['data']['data'][3]
+
+        # Create an object from dataclass
+        z16_model = Z16Model(**hus_info)
+
+        # Get the difference between two record (new - old), converted to dict (asymmetric)
+        info_diff = self.compare_model_diff(z16_model)
+
+        if info_diff:
+            content_msg = self.compose_diff_msg(info_diff)
+            self.bot.send_bot_msg(content_msg)
+
+            # Save the latest info to the object
+            self.previous_model = z16_model
+        else:
+            log.info(f'Model {z16_model.couponCode} price: ${z16_model.finalPrice}/-{z16_model.savePercent}% and shipped in {z16_model.leadTime} days. {z16_model.leadTimeMessage}')
+            log.info(f'ModifyTime: {z16_model.modifyTime}; OnlineTime: {z16_model.onlineDate}; ')
+
+    def get_random_time_gap(self) -> int:
+        # Now time
+        now = datetime.now().time()
+
+        # Lower the speed at night
+        if tm(1,00) <= now <= tm(2,59):
+            time_gap = random.randrange(380, 500)
+        elif tm(3,00) <= now <= tm(7,00):
+            log.info('Task paused...')
+            time_gap = 4*60*60
+        else:
+            time_gap = random.randrange(250, 270)
+        
+        return time_gap
+
+    def compare_model_diff(self, new_model: Z16Model) -> dict:
+        """Compare the new record of model with the saved model"""
+        return dict(new_model.__dict__.items() - self.previous_model.__dict__.items())
+
+    @staticmethod
+    def compose_diff_msg(dct: dict) -> str:
+        msg_content = 'Z16 page change detected!:\n'
+
+        for k, v in dct.items():
+            msg_content += f'📄 {k}: {str(v)} \n'
+        
+        return msg_content
+
+
+class Z16Model(BaseModel):
+    productName: str="NB TP Z16 G1 R7_PRO 32G 2T 11P"
+    productType: str="0"
+    productCode: str="21D4000HUS"
+    productStatus: str=""
+    webPrice: str="5519.00"
+    finalPrice: str="0000.00"
+    couponPrice: str="9999.99"
+    couponSaveAmount: str="1931.65"
+    couponSavePercentage: str="35"
+    saveAmount: str="1931.65"
+    savePercent: str="35"
+    couponCode: str="THINKJUNE"
+    isMedion: bool=False
+    ratingStar: str="0.0"
+    commentCount: int=0
+    marketingStatus: str="Available"
+    modelsCount: int=0
+    leadTime: int=85
+    leadTimeMessage: str="Ships in 3+ months"
+    inventoryStatus: int=1
+    currencySymbol: str="$"
+    currencyCode: str="CAD"
+    modifyTime: datetime=1655700587736
+    onlineDate: datetime=1655390050000
+    machineType: str="22TPZ16Z6A121D4"
+    summary: str="ThinkPad Z16 AMD (16″)"
+    url: str="/p/laptops/thinkpad/thinkpadz/thinkpad-z16-16-amd/21D4000HUS"
+    flCode: str="laptops"
+    flName: str="Laptops"
+    hasClaimIndicator: bool=False
+    maxBuy: int=2
+    maxBuyMessage: str="eCoupon limited to  2 units"
+    couponCount: int=-1
+    couponConsumed: bool=False
+    couponProgress: int=-1
+    couponRuleDiscount: int=35
+    couponDiscountType: int=2
+    isDCGSubseries: bool=False
+
+    @classmethod
+    @property
+    def product_url(cls):
+        return f'https://www.lenovo.com/ca/en{cls.url}'
+
+
+if __name__ == "__main__":
+    LenovoSpider(thread_count=1).start()
+
