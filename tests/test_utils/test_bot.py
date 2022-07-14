265af6458e3249d9ca8c0c63481e5655df0c0772
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-07-14 00:19:01 -0400

    Added test cases for telegram bot update msg method

--- tests/test_utils/test_bot.py
+++ tests/test_utils/test_bot.py
@@ -8,6 +8,8 @@ file_operator = file_input_output.FileReadWrite()
 newbot_token = file_operator.newbot_token
 chat_id = file_operator.chat_id
 
+rfd_token = file_operator.token
+rfd_chat_id = file_operator.channel_id
 
 @pytest.mark.utils
 def test_tgbot():
@@ -24,3 +26,21 @@ def test_tgbot():
     returned_pin_bool = tg_bot.pin_message(pin_message_id=return_msg['message_id'])
 
     assert returned_pin_bool
+
+
+@pytest.mark.utils
+def test_bot_edit_msg():
+    # Instantiate a tg bot instance
+    tg_bot = TelegramBot(rfd_token, rfd_chat_id)
+    
+    test_msg = 'Testing'
+
+    return_msg = tg_bot.send_bot_msg(content_msg=test_msg)
+
+    msg_id = return_msg['message_id']
+    
+    test_msg_2 = 'Testing22222'
+
+    return2_msg = tg_bot.edit_message(content_msg=test_msg_2, edit_msg_id=msg_id)
+
+    assert type(return_msg['message_id']) is int
