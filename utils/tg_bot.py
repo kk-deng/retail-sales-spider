0394a3b47efc7e823ce99f72ccc97e20afbc34db
kk-deng <71301799+kk-deng@users.noreply.github.com>
2022-07-14 00:18:12 -0400

    Added a new funtion in the telegrambot which can update msg

--- utils/tg_bot.py
+++ utils/tg_bot.py
@@ -76,8 +76,8 @@ class TelegramBot:
             return returned_msg
         except Exception as e:
             log.warning(f'## Msg failed sending with error:\n{e}')
-
-            raise Exception
+            return False
+            # raise Exception
     
     def pin_message(self, pin_message_id: int) -> bool:
         """Pin a telegram message with a pin_message_id.
@@ -92,3 +92,55 @@ class TelegramBot:
             chat_id=self.chat_id, 
             message_id=pin_message_id
         )
+
+    def edit_message(
+        self,
+        content_msg: str, 
+        edit_msg_id: str=None,
+        markup_url: str=None,
+    ) -> telegram.Message:
+        """Given a string msg and msg_id, update the sent msg in chat_id.
+
+        Args:
+            content_msg (str): The string msg to send to telegram.
+            edit_msg_id (str or None): The msg_id to be updated.
+
+        Returns:
+            telegram.Message: Return a Message object updated successfully
+        """
+        if markup_url:
+            keyboard = [
+                [
+                    telegram.InlineKeyboardButton("Open Direct Link", url=markup_url),
+                ],
+            ]
+            reply_markup = telegram.InlineKeyboardMarkup(keyboard)
+        else:
+            reply_markup = None
+        
+        # Try to send message to the telegram chat
+        try:
+            if self.parse_mode:
+                returned_msg = self.bot.edit_message_text(
+                    text=content_msg, 
+                    chat_id=self.chat_id,
+                    message_id=edit_msg_id,
+                    reply_markup=reply_markup,
+                    parse_mode=telegram.ParseMode.MARKDOWN
+                )
+            else:
+                returned_msg = self.bot.edit_message_text(
+                    text=content_msg, 
+                    chat_id=self.chat_id,
+                    message_id=edit_msg_id,
+                    reply_markup=reply_markup,
+                )
+            
+            # log.info('## Msg was edited successfully!')
+            time.sleep(3)
+
+            return returned_msg
+        except Exception as e:
+            log.warning(f'## Msg failed sending with error:\n{e}')
+
+            raise Exception
